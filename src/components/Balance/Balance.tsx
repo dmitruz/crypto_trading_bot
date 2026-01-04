043dd4c9b01b3d13b89254bdf4fcfa4b8df10fca
Dmytro Ruzhytskyi <dmitruz970@gmail.com>
2026-01-05 00:20:09 +0200

    update styles in body

--- src/components/Balance/Balance.tsx
+++ src/components/Balance/Balance.tsx
@@ -2,9 +2,11 @@ import "./Balance.scss";
 
 export default function Balance() {
     return (
-        <div className="balance-box">
-            <p>Balance: <strong>$1000</strong></p>
-            <p className="profit">Profit: $0</p>
-        </div >
+        <div className="balance-container">
+            <div className="balance-box">
+                <p>Balance: <strong>$1000</strong></p>
+                <p className="profit">Profit: $0</p>
+            </div >
+        </div>
     )
 }
