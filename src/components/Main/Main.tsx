57bd4a3f05003bebf55b7f418b9f791e8ecc0138
Dmytro Ruzhytskyi <dmitruz970@gmail.com>
2026-01-04 23:50:20 +0200

    add styles for Main section

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,14 @@
+import "./Main.scss";
+
+export default function Main() {
+    return (
+        <main className="main">
+            <h1>Trading Bot FINA</h1>
+
+            <div className="controls">
+                <button className="start">Start Trading</button>
+                <button className="stop">Stop Trading</button>
+            </div>
+        </main>
+    );
+}
