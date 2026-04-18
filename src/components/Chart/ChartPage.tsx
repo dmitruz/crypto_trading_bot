cb25bc5329c94106efcb9ef5e82e0f84fe015f18
Dmytro Ruzhytskyi <dmitruz970@gmail.com>
2026-04-18 16:16:10 +0300

    ubdate priceChart

--- src/components/Chart/ChartPage.tsx
+++ src/components/Chart/ChartPage.tsx
@@ -1,6 +1,6 @@
 
 import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import {
     LineChart,
@@ -11,28 +11,57 @@ import {
     ResponsiveContainer
 } from "recharts";
 
-import { generateChartData } from "./GenerateChartata";
+interface PricePoint {
+    date: string;
+    price: number;
+}
+
+
 
 export default function ChartPage() {
-
     const { symbol } = useParams();
+    const [data, setData] = useState<PricePoint[]>([]);
 
-    const [data] = useState(
-        generateChartData(100 + Math.random() * 200)
-    );
+    useEffect(() => {
+        if (!symbol) return;
+
+        const fetchHistory = async () => {
+            const res = await fetch(`http://localhost:4000/prices/${symbol}`);
+            const history = await res.json();
+
+            const formatted = history.map((point: any) => ({
+                ...point,
+                date: new Date(point.date).toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit"
+                })
+            }));
+
+            setData(formatted);
+        };
+
+        fetchHistory();
+
+        const interval = setInterval(fetchHistory, 10_000);
+
+        return () => clearInterval(interval);
+    }, [symbol]);
+
+    if (!data.length) {
+        return <div>Loading chart...</div>;
+    }
+
 
     return (
         <div style={{ width: "100%", height: "100vh", padding: 40 }}>
-
-            <h1>{symbol} Chart</h1>
-
+            <h2>{symbol} Chart</h2>
             <ResponsiveContainer width="100%" height={400}>
 
                 <LineChart data={data}>
 
                     <XAxis dataKey="time" />
 
-                    <YAxis />
+                    <YAxis domain={["auto", "auto"]} />
 
                     <Tooltip />
 
